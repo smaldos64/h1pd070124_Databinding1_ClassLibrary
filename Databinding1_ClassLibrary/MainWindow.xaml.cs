6352737bbf169b20cf7054b37cad50aafdfea9f5
smaldos64 <gudumholm34@gmail.com>
2024-08-09 09:28:09 +0200

    Add project files.

---
+++ Databinding1_ClassLibrary/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToolsLibrary;
+
+namespace Datbinding1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeStartValuesForTwoWayDataBindingComponents();
+        }
+
+        private void InitializeStartValuesForTwoWayDataBindingComponents()
+        {
+            sldBind1.Value = 30;
+        }
+
+        private void txtCheckForValidKeyPressedPositiveInteger(object sender, KeyEventArgs e)
+        {
+            if (!KeyHelper.IsKeyPressedValidPositiveInteger(e.Key))
+            {
+                SystemSounds.Beep.Play();
+                e.Handled = true;
+            }
+        }
+        
+    }
+}
